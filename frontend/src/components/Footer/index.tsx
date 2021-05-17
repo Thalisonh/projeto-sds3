19f5a6aaa5c215fe85a3d51ce4099d371a4af1a9
thalisonh <thalison_henrique@hotmail.com>
2021-05-17 20:10:13 -0300

    change contact

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Thalison Morais</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Thalisonh" target="_blank" rel="noreferrer">Thalison Morais</a></p>
+                <p className="text-light"><small><strong>Desenvolvido com Spring e React</strong><br />
+      Linkedin: <a href="https://www.linkedin.com/in/thalison-henrique-morais/" target="_blank" rel="noreferrer">Thalison Henrique</a></small></p>
             </div>
         </footer>
     );
